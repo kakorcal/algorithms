64155559a819eb1c0dab4c279dd003e9e5341775
Kenneth Korcal <kakorcal@gmail.com>
2016-06-27 12:22:08 -0700

    insertion sort

--- algorithms/sorting_part1.js
+++ algorithms/sorting_part1.js
@@ -31,7 +31,14 @@ function selectionSort(arr){
 }
 
 function insertionSort(arr){
-
+  for(let i = 0; i < arr.length; i++){
+    let current = arr[i];
+    for(var j = i - 1; j >= 0 && arr[j] > current; j--){
+        arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
 }
 
 module.exports = {swap, bubbleSort, selectionSort, insertionSort};
