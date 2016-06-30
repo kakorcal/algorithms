f94c80c7f3257ac406652fe65305325f548d42e2
Kenneth Korcal <kakorcal@gmail.com>
2016-06-30 12:02:15 -0700

    adding linear search

--- algorithms/search.js
+++ algorithms/search.js
@@ -1,8 +1,12 @@
-Array.prototype.linearSearch = function(){
-  //   
+Array.prototype.linearSearch = function(val){
+  // return the index of this[index] = val;
+  for(let i = 0; i < this.length; i++){
+    if(this[i] === val) return i;
+  }
+  return -1;
 };
 
-Array.prototype.binarySearch = function(){
+Array.prototype.binarySearch = function(val){
 
 };
 
